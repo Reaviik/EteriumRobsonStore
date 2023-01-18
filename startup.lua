d967381323bd59cd7262154dc2b04978d1fd1cdc
Reavik <122496671+Reaviik@users.noreply.github.com>
2023-01-18 14:29:52 -0300

    Started
    All

--- startup.lua
+++ startup.lua
@@ -1,9 +1,12 @@
--- By Reavik, Player_rs and HarryKaray V3.0s
+-- By Reavik and Player_rs V1
 
 local cb = peripheral.find("chatBox")
 if cb == nil then print("chatBox no found") end
+local modem = peripheral.find("modem")
+if modem == nil then print("modem not found") end
+modem.open(7777, 98)
 
-local nameBot = "Robson Store"
+local nameBot = "Robsom"
 local woner = "Reavik"
 local remove = false
 
@@ -16,7 +19,7 @@ local function Split(s, delimiter)
 end
 
 local function loadTabela()
-    local f = fs.open("EteriumSky/priceTable.lua", "r")
+    local f = fs.open(nameBot.."/PriceTable.lua", "r")
     local data = textutils.unserialize(f.readAll())
     f.close()
     return data
@@ -42,107 +45,47 @@ local function findItem(str, table)
 end
 
 local function _downloadList()
-    if not fs.exists("EteriumSky") then
-        fs.makeDir("EteriumSky")
+    if not fs.exists(nameBot) then
+        fs.makeDir(nameBot)
     end
-    shell.run("wget https://raw.githubusercontent.com/Playerrs/CCrepo/master/EteriumLoja/EteriumTable.lua EteriumSky/priceTable.lua")
+    shell.run("wgethttps://raw.githubusercontent.com/Reaviik/EteriumRobsonStore/main/PriceTable.lua "..nameBot.."/PriceTable.lua")
 end
 
 -- Main
-if not fs.exists("EteriumSky/priceTable.lua") then _downloadList() end
+if not fs.exists(nameBot.."/PriceTable.lua") then _downloadList() end
 local tabela = loadTabela()
 print("Tudo certo e funcionando!\n["..nameBot.."]")
 
 while true do
     --editei msg para message
     local e, player, message = os.pullEvent("chat")
-    --modem.transmit(7777,98,{color, player, message})
+    if modem ~= nil then
+        modem.transmit(7777,98,{player, message})
+    end
     local split_string = Split(message, " ")
     msg = (split_string[1]):lower()
 
-    if msg == "help" or msg == "calc" or msg == "c" or msg == "preço" or msg == "price" or msg == "prico" or msg == "sky" or msg == "remove"  or msg == "add" then
+    if msg == "rp" or msg == "remove"  or msg == "add" and modem  ~= nil then
         print("["..player.."]: "..msg)
     end
-    if msg == "preço" or msg == "preco" or msg == "price" then
+    if msg == "rp" then
         local item
         local stats, result
         if split_string[2] then
             item = split_string[2]:lower()
             stats, result = findItem(item, tabela)
         else
-            sendMessage("Para mais informações digit \"sky\" ou \"help\" no chat", player)
+            sendMessage("Digite rp espaço nome do item, Ex: rp iron", player)
         end
 
         if stats then
             for i, v in pairs(result) do
-                sendMessage(("[Tier %s] [%s]:\n %s"):format(v.tier, v.name, v.info), player)
+                sendMessage(("\n[%s]: %s \nAndar: %s"):format(v.name, v.preco, v.andar), player)
                 sleep(1)
             end
         else
-            sendMessage(("[%s] não foi encontrado na tabela\nTente pesquisar por outro nome\nOu calcule o proço baseado nos item usado para o craft\nUse \"help\" para mais informações"):format(item), player)
+            sendMessage(("[%s] Eu ainda não vendo isso, ou você digitou errado\nDigite rpreco no chat local para saber mais."):format(item), player)
         end
-    end
-    if msg == "sky" then
-        sendMessage("------------------ [Eterium Sky]\nExiste um delay de 1s entre um comando e outro\nAss: Reavik, Player_rs", player)
-    end
-    if msg == "help" then
-        sendMessage("----------------- [Eterium Sky]\nDigite no chat local os comandos a seguir sem usar /\nprice\ncalc\nsky\nhelp", player)
-    end
-    -- split_string[1] = calc
-    -- split_string[2] = int
-    -- split_string[3] = parametro
-    -- split_string[4] = int
-    if msg == "calc" or msg == "c" then
-        if split_string[2] then
-            item = split_string[2]:lower()
-            stats, result = findItem(item, tabela)
-            local error = "[X] Você deve digitar apenas numeros e parametros matematicos"
-        if split_string[3] == "+" then
-            if tonumber(split_string[2]) and tonumber(split_string[4]) then
-                sendMessage("A soma de "..split_string[2].." mais "..split_string[4].." é ["..tonumber(split_string[2]) + tonumber(split_string[4]).."]", player)
-            else
-                sendMessage(error,player)
-            end
-        end
-        if split_string[3] == "-" then
-            if tonumber(split_string[2]) and tonumber(split_string[4]) then
-                sendMessage("A subitração de "..split_string[4].." de "..split_string[2].." é ["..tonumber(split_string[2]) - tonumber(split_string[4]).."]", player)
-            else
-                sendMessage(error,player)
-            end
-        end
-        if split_string[3] == "/" or split_string[3] == "%" then
-            if tonumber(split_string[2]) and tonumber(split_string[4]) then
-                sendMessage("A divisão de "..split_string[2].." por "..split_string[4].." é ["..tonumber(split_string[2]) / tonumber(split_string[4]).."]", player)
-            else
-                sendMessage(error,player)
-            end
-        end
-        if split_string[3] == "*" or split_string[3] == "x" then
-            if tonumber(split_string[2]) and tonumber(split_string[4]) then
-                sendMessage("A divisão de "..split_string[2].." por "..split_string[4].." é ["..tonumber(split_string[2]) * tonumber(split_string[4]).."]", player)
-            else
-                sendMessage(error,player)
-            end
-        end
-        if split_string[3] == "^" then
-            if tonumber(split_string[2]) and tonumber(split_string[4]) then
-                sendMessage("A potência de "..split_string[2].." elevado "..split_string[4].." é ["..tonumber(split_string[2]) ^ tonumber(split_string[4]).."]", player)
-            else
-                sendMessage(error,player)
-            end
-        end
-        if split_string[2] == "v" then
-            if tonumber(split_string[3]) then
-                sendMessage("A raiz quadrada de "..split_string[3].." é ["..math.sqrt(tonumber(split_string[3])).."]", player)
-            else
-                sendMessage(error,player)
-            end
-        end
-        else
-            sendMessage("Os parametros validos para a calculadora são:\n+ Soma\n- Subtração\n/ % Divisão\n* x Multilicação\n^ Potência\nv Raiz quadrada", player)
-        end
-        sleep(1)
     end
     if msg == "remove" and player == woner then
         local item
@@ -153,16 +96,16 @@ while true do
             item = split_string[2]:lower()
             stats, result = findItem(item, tabela)
         else
-            sendMessage("Digite remove nomeDoItem para remover um item.", player)
+            sendMessage("Digite remove NomeDoItem para remover um item.", player)
         end
 
         if stats and remove == false then
             for i, v in pairs(result) do
-                sendMessage(("%s: [Tier %s] [%s]:\n %s"):format(v.index, v.tier, v.name, v.info), player)
+                sendMessage(("Index: %s\n[%s]: %s \nAndar: %s"):format(v.index, v.name, v.preco, v.andar), player)
                 sleep(1)
                 --add o resultado da busca a uma tabela
             end
-            sendMessage("Qual desses item voçe deseja remover", player)
+            sendMessage("Digite o Index do item que deseja remover", player)
             remove = true
         elseif stats and remove == true then
             print("Removendo")
@@ -170,7 +113,7 @@ while true do
             table.remove(tabela, tonumber(split_string[2]))
 
             --ler o arquivo com reescrever
-            local f = fs.open("EteriumSky/priceTable.lua", "w")
+            local f = fs.open(nameBot.."/PriceTable.lua", "w")
 
             --reescreve a tabela
             f.write(textutils.serialise(tabela))
@@ -190,23 +133,25 @@ while true do
         --split 3 valores "mane" "info" "tier" e separas as palavras
         local name = string.gsub(split_string[1], "(%u)", " %1"):gsub("^%*", "",1)
         --R preço unitario / pack
-        local info = string.gsub(split_string[2], "(%u)", " %1")
+        local preco = tonumber(split_string[2])
         --R andar
-        local tier = tonumber(split_string[3])
+        local andar = tonumber(split_string[3])
+        --R stack
+        local stack = preco * 64
         --adicionar a tabela
-            if type(tier) == "number" then
-                table.insert(tabela,{name = name, info = info, tier = tier})
+            if type(andar) == "number" and type(preco) == "number" then
+                table.insert(tabela,{name = name, preco = preco, andar = andar})
                 --gravar a nova tabela
-                local f = fs.open("EteriumSky/priceTable.lua", "w")
+                local f = fs.open(nameBot.."/PriceTable.lua", "w")
                 f.write(textutils.serialise(tabela))
                 --fexar a tabela
                 f.close()
-                sendMessage("O item "..(name:gsub("^%*", "",1)).." foi adicionado a tabela por "..info.." no tier "..info, player)
+                sendMessage("O item "..(name:gsub("^%*", "",1)).." foi adicionado a tabela por "..preco.." no andar "..andar, player)
             else
-                sendMessage("O tier precisa ser um numero")
+                sendMessage("O andar precisa ser um numero!")
         end
         else
-            sendMessage("Você deve digitar \"add name, info, tier\"", player)
+            sendMessage("Você deve digitar: \"add name, preço, andar\"", player)
         end
     end
 end
